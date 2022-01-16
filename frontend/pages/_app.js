54ae9578ce8d87ffe7199a715de45198a9d215d7
Roderick Trice <rocky_trice@yahoo.com>
2022-01-16 01:16:49 -0500

    Global styles, Typography, and layout styles.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,14 @@
 import { Component } from 'react';
+import Nprogress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+// Todo: swap with our own
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => Nprogress.start());
+Router.events.on('routeChangeComplete', () => Nprogress.done());
+Router.events.on('routeChangeError', () => Nprogress.done());
 
 export default function Myapp({ Component, pageProps }) {
   return (
