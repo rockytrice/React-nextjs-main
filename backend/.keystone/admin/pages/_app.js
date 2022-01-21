ccb38aaeb9c8774ee36622f199ee7e687b5bb328
Roderick Trice <rocky_trice@yahoo.com>
2022-01-21 00:08:18 -0500

    adding auth to keystone app

--- backend/.keystone/admin/pages/_app.js
+++ backend/.keystone/admin/pages/_app.js
@@ -7,10 +7,11 @@ import { Core } from '@keystone-ui/core';
 import * as viewba74eeb6 from '../../../node_modules/@keystone-next/fields/types/mongoId/views';
 import * as view8ab1740c from '../../../node_modules/@keystone-next/fields/types/text/views';
 import * as viewa9491b98 from '../../../node_modules/@keystone-next/fields/types/password/views';
+import * as view48436935 from '../../../node_modules/@keystone-next/fields/types/timestamp/views';
 
 const adminConfig = {};
 
-const fieldViews = { viewba74eeb6, view8ab1740c, viewa9491b98 };
+const fieldViews = { viewba74eeb6, view8ab1740c, viewa9491b98, view48436935 };
 
 const lazyMetadataQuery = {
   kind: 'Document',
@@ -150,6 +151,29 @@ const lazyMetadataQuery = {
             },
             loc: { start: 22, end: 234 },
           },
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'authenticatedItem' },
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {
+                  kind: 'InlineFragment',
+                  typeCondition: {
+                    kind: 'NamedType',
+                    name: { kind: 'Name', value: 'User' },
+                  },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
         ],
       },
     },
@@ -161,7 +185,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="1o4w33y"
+        adminMetaHash="109zwrc"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,10 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 
 const databaseURL =
@@ -10,26 +15,44 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  // boiler plate
-  server: {
-    cors: {
-      orgin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // Todo add in inital roles here
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: Add data seeding here
-  },
-  lists: createSchema({
-    // Schema items go here
-    User,
-  }),
-  ui: {
-    // TODO change this for role
-    isAccessAllowed: () => true,
-  },
-  // TODO add session values here
 });
+
+export default withAuth(
+  config({
+    // boiler plate
+    server: {
+      cors: {
+        orgin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // Schema items go here
+      User,
+    }),
+    ui: {
+      // Show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    // TODO add session values here
+    session: withItemData(statelessSessions(sessionConfig), {
+      // this will pass the id and any other data that we query along with every session. Anytime we want to access the sesion we will have access to the user's name,email,ID etc.
+      User: 'id name email',
+    }),
+  })
+);
