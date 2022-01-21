5833bc6709ec3ae58513c53bf3915e63a64102d4
Roderick Trice <rocky_trice@yahoo.com>
2022-01-20 23:40:23 -0500

    Setting up first user data type

---
+++ backend/keystone.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  // boiler plate
+  server: {
+    cors: {
+      orgin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: Add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go here
+    User,
+  }),
+  ui: {
+    // TODO change this for role
+    isAccessAllowed: () => true,
+  },
+  // TODO add session values here
+});
